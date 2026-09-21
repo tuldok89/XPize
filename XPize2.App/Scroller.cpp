4bd79a22ac285286ae79f19ec5a679bc4b3edd9a
Tuldok <tuldok89@gmail.com>
2026-09-21 23:30:02 +0800

    Fix mouse scrolling

--- XPize2.App/Scroller.cpp
+++ XPize2.App/Scroller.cpp
@@ -51,8 +51,9 @@ void Scroller::OnMouseWheel(wxMouseEvent& event)
 		return;
 	}
 
+	int posX = 0;
 	int posY = 0;
-	GetViewStart(nullptr, &posY);
+	GetViewStart(&posX, &posY);
 
 	long delta = static_cast<long>(rotation) * 64 * event.GetLinesPerAction() / wheelDelta;
 	long pos = posY - delta;
@@ -60,7 +61,7 @@ void Scroller::OnMouseWheel(wxMouseEvent& event)
 
 	if (pos != posY)
 	{
-		Scroll(0, static_cast<int>(pos));
+		Scroll(posX, static_cast<int>(pos));
 	}
 }
 
